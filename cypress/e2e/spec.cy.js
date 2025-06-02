60bb540d156f2ea508294e84baea0e1f7b7e0a7f
cecijunq <cicijunqueira@hotmail.com>
2025-06-02 10:37:05 -0300

    Finalizado a produção de 3 novos testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,63 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  it('Não adiciona tarefas vazias', () => {
+    cy.visit('');
+  
+    cy.get('[data-cy=todo-input]').type('{enter}');
+  
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0); // Nenhuma tarefa deve ser adicionada
+  });
+
+  it('Marca e desmarca uma tarefa como concluída', () => {
+    cy.visit('');
+  
+    cy.get('[data-cy=todo-input]')
+      .type('Estudar Cypress{enter}');
+  
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .check()
+      .should('be.checked');
+  
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .should('have.class', 'completed'); // Verifica se aplicou a classe
+  
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .uncheck()
+      .should('not.be.checked');
+  
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .should('not.have.class', 'completed');
+  });
+
+  it('Remove apenas a tarefa clicada', () => {
+    cy.visit('');
+  
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 Engenharia de Software{enter}')
+      .type('TP2 ICV{enter}');
+  
+    cy.get('[data-cy=todos-list] > li')
+      .should('have.length', 2);
+  
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .within(() => {
+        cy.get('[data-cy=remove-todo-btn]')
+          .invoke('show')
+          .click();
+      });
+  
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 ICV');
+  });
+  
 });
